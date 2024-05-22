df4f5e915ce2b7cf19a44276e6cbfb4f52f99629
hamid <hosinihamid187@gmail.com>
2024-05-22 22:38:12 +0430

    Modal Component

---
+++ Modal/readme.md
@@ -0,0 +1,4 @@
+# Modal 🌟
+## HTML CSS JS
+
+## Follow me on Telegram [@DvLpEr](https://T.me/DvLpEr)
---
+++ Modal/script.js
@@ -0,0 +1,16 @@
+const modal = document.querySelector(".modal-container");
+const openModal = document.querySelector("#openBtn");
+const closeModal = document.querySelector(".closeBtn");
+
+openModal.addEventListener('click' , ()=>{
+    modal.style.display = "block";
+});
+
+closeModal.addEventListener('click' , ()=>{
+    modal.style.display = "none";
+});
+window.addEventListener('click' , (e)=>{
+    if(e.target == modal){
+        modal.style.display="none";
+    }
+});
