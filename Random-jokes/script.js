7ad025cfaf838f5fbe05b1f86a0201c39df99b15
hamid <hosinihamid187@gmail.com>
2024-07-04 16:48:19 +0430

    Random Jokes

---
+++ Random-jokes/script.js
@@ -0,0 +1,15 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', ()=>{
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+  fetch('https://icanhazdadjoke.com', config)
+    .then((res) => res.json())
+    .then((data) => {
+      jokeEl.innerHTML = data.joke
+    })
+  });
